ff95cc751b8a9015e8079572eae550b95a768dd1
Gabriel Nobre Viana <gabriel.viana@telecall.com>
2023-02-01 12:42:31 -0300

    teste

---
+++ Models/BancoDeDados.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Teste.Models
+{
+    public class BancoDeDados : DbContext
+
+    {
+        public DbSet<Cliente>clientes{get; set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"server=(localdb)\mssqllocaldb;Database=Teste;Integrated Security=True");
+        }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace Teste.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+
+        public string Telefone { get; set; }
+
+
+
+    }
+}
